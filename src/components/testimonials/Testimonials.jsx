4e48317a54c716b34066650993deff235229dca0
JacobWik <jacob.wik.96@gmail.com>
2021-12-08 20:38:53 +0100

    added functionality to sections 2 and 3, portfolio section and slider

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,31 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
-            
+            <div className="testimonials" id="testimonials">
+                <h1>Testimonials</h1>
+                <div className="container">
+                    <div className="card">
+                        <div className="top">
+                            <img src="assets/right-arrow.png" className="left" alt="" />
+                            <img
+                                className="user"
+                                src=""//{d.img}
+                                alt=""
+                            />
+                            <img className="right" src="" alt=""/>
+                        </div>
+                        <div className="center">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi magnam aperiam,
+                             eius sunt fuga maxime? Nostrum quo reiciendis facilis!
+                        </div>
+                        <div className="bottom">
+                            <h3>Alex</h3>
+                            <h4>CEO of Microsoft</h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
