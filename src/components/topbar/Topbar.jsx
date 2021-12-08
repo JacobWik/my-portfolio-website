5b3b8b17399770f23cf5a663ba1bee49bea5b114
JacobWik <jacob.wik.96@gmail.com>
2021-12-08 15:57:47 +0100

    added a intro and topbar

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,38 @@
+import "./topbar.scss";
+import {Person, Mail} from "@material-ui/icons";
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")}>
+
+            <div className="wrapper">
+
+                <div className="left">
+
+                    <a href="#intro" className="logo">
+                        Genius.
+                    </a>
+
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span> 070-947 60 89</span>
+                    </div>
+
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span> Jacob.Wik.96@gmail.com</span>
+                    </div>
+
+                </div>
+
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
